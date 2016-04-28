48024269650f7f6d91661bd8983b99a3571e39ef
David Kormushoff <kormie@gmail.com>
2016-04-28 18:12:18 -0500

    Updates

--- app/routes/room.js
+++ app/routes/room.js
@@ -1,26 +1,36 @@
-import Ember from 'ember';
-
+import Ember from 'ember'; 
 export default Ember.Route.extend({
   model() {
     return this.messages;
   },
   channel: null,
   messages: null,
+
+
+
+
+
   init: function() {
     let socket = this.get('phoenix').socket();
     let room = socket.channel("rooms:lobby", {});
-    this.store.findAll('message').then((messages) =>{
-      this.set('messages', messages);
-    });
+    this.setupMessages();
+
     this.set('channel', room);
+
+
+
     room.join().receive('ok', (data) => {
       this.store.unloadAll('message');
       data.messages.forEach((messageData) => {
         this.renderMessage(messageData);
-        this.refresh();
       });
+      this.refresh();
     });
+
+
     room.on("new:message", msg => this.renderMessage(msg));
+
+
   },
   renderMessage: function(data) {
     let messageData = data.message;
@@ -34,6 +44,7 @@ export default Ember.Route.extend({
         }
       }
     });
+
     this.messages.pushObject(newMessage._internalModel);
   },
   actions: {
@@ -42,5 +53,10 @@ export default Ember.Route.extend({
       this.set('newMessage', null);
       this.controller.set('newMessage', '');
     }
+  },
+  setupMessages() {
+    this.store.findAll('message').then((messages) =>{
+      this.set('messages', messages);
+    });
   }
 });
